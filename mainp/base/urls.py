ac19591f1dab49e86750dc0f43deecd4af4b3666
DCNTT <99235494+DCNTT@users.noreply.github.com>
2022-03-09 13:06:35 +0900

    changed

--- mainp/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns =[
-    path('', views.login, name='login'),
-    path('reg/', views.reg, name = 'reg'),
-    path('shift/', views.shift, name = 'shift'),
-    path('add/', views.add, name = 'add'),
-]
